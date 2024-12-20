19cef883916ad2db9845af9c97b9e21835acec6a
remeyesek <rmyskrts@gmail.com>
2024-12-20 21:22:39 +0300

    remeyese.R

--- dependence.R
+++ dependence.R
@@ -53,60 +53,8 @@ newsystcomp=systLoB1[i,]+LoB1unobserved[i,]
 #systematic component for additive model
 
 newsystcomp=systLoB2[i,]*LoB2unobserved[i,]
-
 #systematic component for multiplicative model
 
 cellerror_LoB1=sqrt( (sum((LoB1losses - LoB1losses_predicted)^2)) / (sum(LoB1losses_predicted^2)))
-#cell error calculatiob for LoB1
+#cell error calculation for LoB1
 
-
-
-#############################################################
-######################################
-
-copparam=copulaparameter
-mycop=normalCopula(copparam,dim=2,dispstr="ex")
-loglikCOPULA=function(thetarm,py,px,cy,cx,copula){
-  copulaparam=thetarm[1]
-  beta1=thetarm[2:20]
-  beta2=thetarm[21:39]
-  loglik1=loglikNORM(beta1,disp=2.434273,py=persyvariate,px=persxvariate)
-  loglik2=loglikGAMMA(beta2,disp=0.7664748,cy=lnsiz,cx=comsxvariate)
-  uu=cbind(loglik1[[2]],loglik2[[2]])
-  personalloglik=loglik1[[1]]
-  commercialloglik=loglik2[[1]]
-  copula@parameters=copulaparam
-  Loglik=personalloglik+commercialloglik+sum(log(dCopula(uu,copula=mycop)))
-  return(-Loglik)
-}
-#############################################################
-######################################
-
-
-
-set.seed(123)
-simulated_data <- rCopula(1000, copula_fit@copula)
-
-cat("LoB1 Ortalama Rezerv Tahmini: ", mean_reserve_LoB1, "\n")
-cat("LoB2 Ortalama Rezerv Tahmini: ", mean_reserve_LoB2, "\n")
-
-LoB1CL <- MackChainLadder(LoB1_loss_triangle)
-LoB2CL <- MackChainLadder(LoB2_loss_triangle)
-#CL estimation
-
-LoB1fixed=paramt[1]
-kayiki=kpar[2]
-kayuc=kpar[3]
-kaydort=kpar[4]
-kaybes=kpar[5]
-kayalti=kpar[6]
-kayyedi=kpar[7]
-kdyiki=kpar[8]
-kdyuc=kpar[9]
-kdydort=kpar[10]
-kdybes=kpar[11]
-kdyalti=kpar[12]
-kdyyedi=kpar[13]
-
-systcomp1= fixed value+ accident year+ development year
-#systematic components for LoB1
